406706e4d46356f9a15a72ba2210db2e6715ca20
andrea <andrealisi88@gmail.com>
2022-12-05 17:17:28 +0100

    delete build directory

--- build/kconfig_bin/expand_env.d
+++
@@ -1,4 +0,0 @@
-expand_env.o: \
- /home/andrea/esp_8266/ESP8266_RTOS_SDK/tools/kconfig/expand_env.c \
- /home/andrea/esp_8266/ESP8266_RTOS_SDK/tools/kconfig/expand_env.h
-/home/andrea/esp_8266/ESP8266_RTOS_SDK/tools/kconfig/expand_env.h:
--- build/kconfig_bin/lxdialog/menubox.d
+++
@@ -1,4 +0,0 @@
-lxdialog/menubox.o: \
- /home/andrea/esp_8266/ESP8266_RTOS_SDK/tools/kconfig/lxdialog/menubox.c \
- /home/andrea/esp_8266/ESP8266_RTOS_SDK/tools/kconfig/lxdialog/dialog.h
-/home/andrea/esp_8266/ESP8266_RTOS_SDK/tools/kconfig/lxdialog/dialog.h:
